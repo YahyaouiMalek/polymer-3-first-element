4597eea88c1e078dd358abb4793f1866e2f5c54c
Malek <yahyaoui.malek@gmail.com>
2018-05-24 08:47:37 +0100

    My initial version

---
+++ icon-toggle.js
@@ -0,0 +1,52 @@
+import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
+import '@polymer/iron-icon/iron-icon.js';
+
+class IconToggle extends PolymerElement {
+  static get template() {
+    return html`
+      <style>
+        /* shadow DOM styles go here */
+        :host {
+          display: inline-block;
+          --icon-toggle-color: lightgrey;
+          --icon-toggle-outline-color: black;
+          --icon-toggle-pressed-color: red;
+        }
+        iron-icon {
+          fill: var(--icon-toggle-color, rgba(0,0,0,0));
+          stroke: var(--icon-toggle-outline-color, currentcolor);
+          cursor: pointer;
+        }
+        :host([pressed]) iron-icon {
+          fill: var(--icon-toggle-pressed-color, currentcolor);
+        }
+      </style>
+  
+      <!-- shadow DOM goes here -->
+      <iron-icon icon="[[toggleIcon]]"></iron-icon>
+
+    `;
+  }
+  static get properties() {
+    return {
+      pressed: {
+        type: Boolean,
+        value: false,
+        notify: true,
+        reflectToAttribute: true
+      },
+      toggleIcon: {
+        type: String
+      }
+    };
+  }
+  constructor() {
+    super();
+    this.addEventListener('click', this.toggle.bind(this));
+  }
+  toggle() {
+    this.pressed = !this.pressed;
+  }
+}
+
+customElements.define('icon-toggle', IconToggle);
